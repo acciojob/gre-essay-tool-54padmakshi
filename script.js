0b1a01127b02bd7a99204924696cc186b9d6aeaf
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2022-11-26 18:05:00 +0530

    updated

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function myFunction() {
     
     var x = document.getElementById("evaluatedText").value;
     var a = x.split(" ");
-    console.log(a);
+   
     document.getElementById("wordCount").innerHTML =  a.length;
   }

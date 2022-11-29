d695990a58f17540105d1dbe2943b1976cd1b949
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2022-11-29 11:23:25 +0530

    updated

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+
+
 //your code here
 function myFunction() {
     
     var x = document.getElementById("evaluatedText").value;
-    var a = x.split(" ");
-   
-    document.getElementById("wordCount").innerHTML =  a.length;
-  }
+    var a = x.trim();
+    if (a.length == 0) {
+      document.getElementById("wordCount").innerHTML =  0;
+      return;
+    }
+    var b= a.split(" "); 
+    
+   // console.log(b);
+    document.getElementById("wordCount").innerHTML =  b.length;
+}
